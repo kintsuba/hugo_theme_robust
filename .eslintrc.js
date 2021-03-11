a82207bb689a42457820ef9f623d364d09906a72
n-nagai <n-nagai@situs.co.jp>
2021-03-12 01:28:29 +0900

    add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: "eslint:recommended",
+  parser: "",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {},
+};
